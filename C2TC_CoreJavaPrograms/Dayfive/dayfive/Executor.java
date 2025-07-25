0c3b74b9ebb630f1ae56fe9c20bde32a74ee6177
Vishwa658 <HP@LAPTOP-RDRV1JMU.mshome.net>
2025-07-25 15:37:12 +0530

    daysix

---
+++ C2TC_CoreJavaPrograms/Dayfive/dayfive/Executor.java
@@ -0,0 +1,9 @@
+package dayfive;
+
+public class Executor {
+	public static void main (String[] args) {
+		Trainer t1=new Trainer(112,"shiva","Tn","JFS,"lead","C67");
+			System.out.println(t1);
+	}
+
+}
